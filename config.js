08412ac62a4bc8da60b27fa80e37960e774a6a77
tepiweb0-hash <tepiweb0@gmail.com>
2026-09-12 10:01:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.TP_CONFIG={API_URL:"PASTE_APPS_SCRIPT_EXEC_URL_HERE"};
+window.TP_CONFIG={API_URL:"https://script.google.com/macros/s/AKfycbyRfQGCdNNDNGxWB6G2QWILY8pyak0ZIQ55BCxaCSVVCYcabEosZCNC_fjQVsGCHNOoBw/exec"};
