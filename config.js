ca2fc47caa9527152b25b86c72edb6071a88bf10
tepiweb0-hash <tepiweb0@gmail.com>
2026-09-12 04:18:54 +0000

    Delete all files

--- config.js
+++
@@ -1,4 +0,0 @@
-window.TP_CONFIG={
-  API_URL:"https://script.google.com/macros/s/AKfycbxa5Asq89nfpza94FrxOIi-27hGiIH0XFj_bVwZYolOWrlbIu9stKMq_62G1qLvO1ABww/exec",
-  SITE_URL:"https://tepisclovernotes.vercel.app"
-};
