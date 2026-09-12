f136ff66a74697e4f12b7de8993902652930fe77
tepiweb0-hash <tepiweb0@gmail.com>
2026-09-12 10:41:46 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.TP_CONFIG={
-  API_URL:"https://script.google.com/macros/s/AKfycbxa5Asq89nfpza94FrxOIi-27hGiIH0XFj_bVwZYolOWrlbIu9stKMq_62G1qLvO1ABww/exec",
+  API_URL:"https://script.google.com/macros/s/AKfycbyRfQGCdNNDNGxWB6G2QWILY8pyak0ZIQ55BCxaCSVVCYcabEosZCNC_fjQVsGCHNOoBw/exec",
   SITE_URL:"https://tepisclovernotes.vercel.app"
 };
