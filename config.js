3350690dfc70d044f209d699cc1e9ee8ee5e3065
tepiweb0-hash <tepiweb0@gmail.com>
2026-09-12 11:52:37 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.TP_CONFIG={
+  API_URL:"https://script.google.com/macros/s/AKfycbxa5Asq89nfpza94FrxOIi-27hGiIH0XFj_bVwZYolOWrlbIu9stKMq_62G1qLvO1ABww/exec",
+  SITE_URL:"https://tepisclovernotes.vercel.app"
+};
